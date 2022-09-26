8470454c9d59a5d713e8f003963f615c2366fbeb
Ferhan Syed <syedferhan10@gmail.com>
2022-09-26 16:34:08 -0500

    Add data to db and fix syntax issues

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    def _str_(self):
+    def __str__(self):
         return self.question_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
@@ -15,6 +15,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes=models.IntegerField(default=0)
-    def _str_(self):
+    def __str__(self):
         return self.choice_text
 
